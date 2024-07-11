14e437f608c93d45bc834915bc9539812a7da321
Harry Townsend <harry.james.townsend@gmail.com>
2024-07-11 22:23:20 +0100

    Added header row preservation to "exerpt".

--- exerpt.py
+++ exerpt.py
@@ -2,10 +2,25 @@ import argparse
 import os
 from pydat import *
 
+def writeLines(input, output, count):
+	for line in input:
+		output.write(line)
+
+		count = count - 1
+		if count == 0:
+			break
+
+def skipLines(input, count):
+	for line in input:
+		count = count - 1
+		if count == 0:
+			break
+
 parser = argparse.ArgumentParser()
 parser.add_argument("file", type = str, help = "The file to extract lines from.")
 parser.add_argument("-l", "--lines", type = int, default = 100, help = "The number of lines to take from the file.")
 parser.add_argument("-o", "--offset", type = int, default = 0, help = "The number of lines to skip at the start of the file.")
+parser.add_argument("-hr", "--header", type = int, default = 0, help = "The number of header rows to keep.")
 args = parser.parse_args()
 
 if not os.path.exists(args.file):
@@ -32,16 +47,12 @@ else:
 
 with open(args.file, "r") as input:
 	with open(outFile, "w") as output:
-		count = 0
-		while count < args.offset and input.readline() is not None:
-			count = count + 1
+		if args.header > 0:
+			writeLines(input, output, args.header)
 
-		count = 0
-		for line in input:
-			output.write(line)
+		if args.offset > 0:
+			skipLines(input, args.offset)
 
-			count = count + 1
-			if count >= args.lines:
-				break
+		writeLines(input, output, args.lines)
 
 print("Created file: " + outFile)
